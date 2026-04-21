73d1b23551e1bec52958f5a24fc6f14b8444962c
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-04-21 18:40:04 +0200

    Add event listener to toggle section background

--- 3ºTr/prueba-js/script.js
+++ 3ºTr/prueba-js/script.js
@@ -4,3 +4,10 @@ const menuUl = document.querySelector(".contenido")
 iconMenu.addEventListener("click", () => {
     menuUl.classList.toggle("mostrar")
 })
+
+const section = document.querySelector(".section")
+const btnSection = document.querySelector("button")
+
+btnSection.addEventListener("click", () => {
+    section.classList.toggle("nuevo-fondo")
+})
