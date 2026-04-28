1bb27f13c473248256626876bb333e0bf79433f6
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-04-28 12:42:18 +0200

    ejercicios 28A

--- 3ºTr/prueba-js/script.js
+++ 3ºTr/prueba-js/script.js
@@ -8,6 +8,13 @@ iconMenu.addEventListener("click", () => {
 const section = document.querySelector(".section")
 const btnSection = document.querySelector("button")
 
+const listaColores = ["#0b479471", "#C8E6C9", "#f8e8bb", "#E1BEE7", "#add8e663"   ]
+let i = 0
+
 btnSection.addEventListener("click", () => {
-    section.classList.toggle("nuevo-fondo")
+    section.style.backgroundColor = listaColores[i]
+    i++
+    if (i >= listaColores.length) {
+        i = 0
+    }
 })
