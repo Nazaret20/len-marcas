3f3445055e332376297936be1d3e18aefb5660b4
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-05-29 13:56:47 +0200

    cambios reseñas

--- 3ºTr/tareaEntregableJS/script.js
+++ 3ºTr/tareaEntregableJS/script.js
@@ -5,3 +5,11 @@ const menuContenido = document.querySelector(".nav-contenido");
 iconMenu.addEventListener("click", () => {
     menuContenido.classList.toggle("mostrar");
 })
+
+// Selecciona todos los corazones de la tienda
+const corazones = document.querySelectorAll('.like');
+
+
+  corazon.addEventListener('click', () => {
+    corazon.classList.toggle('favorito');
+  });
