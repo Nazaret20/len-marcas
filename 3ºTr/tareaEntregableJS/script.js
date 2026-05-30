c9b32c8dd9d0e355b58cb86b14b43cc28d2caff6
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-05-30 18:48:25 +0200

    entregable finalizado

--- 3ºTr/tareaEntregableJS/script.js
+++ 3ºTr/tareaEntregableJS/script.js
@@ -3,13 +3,54 @@ const iconMenu = document.querySelector(".hamburguesa");
 const menuContenido = document.querySelector(".nav-contenido");
 
 iconMenu.addEventListener("click", () => {
-    menuContenido.classList.toggle("mostrar");
+  menuContenido.classList.toggle("mostrar");
+});
+
+/*-------------Productos----------------*/
+const corazones = document.querySelectorAll(".like");
+//Necesitamos un foreach porque no es solo un like, sino varios, si no pusieramos foreach solo vería un like, el primero
+corazones.forEach((corazon) => {
+  corazon.addEventListener("click", () => {
+    corazon.classList.toggle("favorito");
+  });
+});
+
+const btnAnadir = document.querySelectorAll(".btn-anadir");
+//Una alerta para que el usuario tenga feedback de que al pulsar, el evento hizo su función
+btnAnadir.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    alert("¡El producto se añadió al carrito!");
+  });
+});
+
+const suscribir = document.querySelector(".btn-sub");
+//En todas las páginas no existe este botón, por tanto da null y así lo protegemos para que no rompa el script
+if (suscribir) {
+  suscribir.addEventListener("click", () => {
+    alert("¡Te has suscrito con éxito!")
+  })
+}
+
+const imagenes = document.querySelectorAll('.tarjeta img')
+    //Efecto mouseover para quitar y poner el "zoom" a la imagen
+  imagenes.forEach(imagen => {
+    imagen.addEventListener('mouseover', () => {
+      imagen.style.transform = 'scale(1.08)'
+      imagen.style.transition = 'transform 0.3s ease'
+    })
+
+    imagen.addEventListener('mouseout', () => {
+      imagen.style.transform = 'scale(1)'
+    })
 })
 
-// Selecciona todos los corazones de la tienda
-const corazones = document.querySelectorAll('.like');
 
+/*-----------Subir página------------*/
+const btnSubir = document.querySelector(".btn-subir")
 
-  corazon.addEventListener('click', () => {
-    corazon.classList.toggle('favorito');
-  });
+btnSubir.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  })
+})
