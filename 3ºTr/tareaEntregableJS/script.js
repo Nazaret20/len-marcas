f81879588bc18483f91bf9b86306740239d23130
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-06-01 09:59:42 +0200

    añadido evento submit

--- 3ºTr/tareaEntregableJS/script.js
+++ 3ºTr/tareaEntregableJS/script.js
@@ -54,3 +54,19 @@ btnSubir.addEventListener("click", () => {
     behavior: "smooth",
   })
 })
+
+/*------------Formulario-------------*/
+const form = document.querySelector(".form")
+const inputEmail = document.getElementById("email")
+const mensajeExito = document.querySelector(".mensaje-exito")
+
+if (form) {
+  //Para recordar que el evento se coloca al formulario en sí, no al botón
+  form.addEventListener('submit', (evento) => {
+    //Con esto evitamos que la página envíe y se recargue
+    evento.preventDefault()
+    mensajeExito.style.color = "rgb(190, 186, 151)"
+    mensajeExito.textContent = "¡Formulario enviado correctamente!"
+    form.reset()
+  })
+}
