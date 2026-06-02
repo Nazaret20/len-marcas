199cd2ad6aeb87a74ba15110ba7e1c08dc5ce1bf
Nazaret <nazaretgutirrezmartn@gmail.com>
2026-06-02 13:58:44 +0200

    mejora corazón js

--- 3ºTr/tareaEntregableJS/script.js
+++ 3ºTr/tareaEntregableJS/script.js
@@ -1,29 +1,29 @@
 /*--------------Cambios globales--------------*/
-const iconMenu = document.querySelector(".hamburguesa");
-const menuContenido = document.querySelector(".nav-contenido");
+const iconMenu = document.querySelector(".hamburguesa")
+const menuContenido = document.querySelector(".nav-contenido")
 
 iconMenu.addEventListener("click", () => {
-  menuContenido.classList.toggle("mostrar");
-});
+  menuContenido.classList.toggle("mostrar")
+})
 
 /*-------------Productos----------------*/
-const corazones = document.querySelectorAll(".like");
+const corazones = document.querySelectorAll(".like")
 //Necesitamos un foreach porque no es solo un like, sino varios, si no pusieramos foreach solo vería un like, el primero
 corazones.forEach((corazon) => {
   corazon.addEventListener("click", () => {
-    corazon.classList.toggle("favorito");
-  });
-});
+    corazon.classList.toggle("favorito")
+  })
+})
 
-const btnAnadir = document.querySelectorAll(".btn-anadir");
+const btnAnadir = document.querySelectorAll(".btn-anadir")
 //Una alerta para que el usuario tenga feedback de que al pulsar, el evento hizo su función
 btnAnadir.forEach((boton) => {
   boton.addEventListener("click", () => {
-    alert("¡El producto se añadió al carrito!");
-  });
-});
+    alert("¡El producto se añadió al carrito!")
+  })
+})
 
-const suscribir = document.querySelector(".btn-sub");
+const suscribir = document.querySelector(".btn-sub")
 //En todas las páginas no existe este botón, por tanto da null y así lo protegemos para que no rompa el script
 if (suscribir) {
   suscribir.addEventListener("click", () => {
@@ -32,22 +32,22 @@ if (suscribir) {
 }
 
 const imagenes = document.querySelectorAll('.tarjeta img')
-    //Efecto mouseover para quitar y poner el "zoom" a la imagen
-  imagenes.forEach(imagen => {
-    imagen.addEventListener('mouseover', () => {
-      imagen.style.transform = 'scale(1.08)'
-      imagen.style.transition = 'transform 0.3s ease'
-    })
+//Efecto mouseover para quitar y poner el "zoom" a la imagen
+imagenes.forEach(imagen => {
+  imagen.addEventListener('mouseover', () => {
+    imagen.style.transform = 'scale(1.08)'
+    imagen.style.transition = 'transform 0.3s ease'
+  })
 
-    imagen.addEventListener('mouseout', () => {
-      imagen.style.transform = 'scale(1)'
-    })
+  imagen.addEventListener('mouseout', () => {
+    imagen.style.transform = 'scale(1)'
+  })
 })
 
 
 /*-----------Subir página------------*/
 const btnSubir = document.querySelector(".btn-subir")
-
+//Evento para ventana que sube a top 0
 btnSubir.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
@@ -62,7 +62,7 @@ const mensajeExito = document.querySelector(".mensaje-exito")
 
 if (form) {
   //Para recordar que el evento se coloca al formulario en sí, no al botón
-  form.addEventListener('submit', (evento) => {
+  form.addEventListener("submit", (evento) => {
     //Con esto evitamos que la página envíe y se recargue
     evento.preventDefault()
     mensajeExito.style.color = "rgb(190, 186, 151)"
